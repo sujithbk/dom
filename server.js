64270b26c90bc98d0488a605928951e2ce3be022
Sujith S <sujithsathosh710@gmail.com>
2024-01-10 19:23:49 +0530

    dom first

---
+++ server.js
@@ -0,0 +1,28 @@
+/// DOM by ID
+
+// document.getElementById("line").innerHTML="START"
+// document.getElementById("line").style.color="blue"
+
+// /// DOM by CLASS
+
+// let clas = document.getElementsByClassName("cls")
+// clas[0].style.color="red"
+
+// /// DOM by TAGNAME
+
+// let tg = document.getElementsByTagName("a")
+// tg[0].textContent="OK!!"
+
+// /// DOM by CSS SELECTORS
+
+// document.querySelector("#map").innerHTML = "MOON"
+// let qc = document.querySelectorAll("span")
+// qc[0].style.color="orange"
+
+///  form selectors
+
+// let ab = document.forms["forms1"]
+// console.log(ab);
+
+// let res = document.getElementById("hre").herf = "https://www.google.com/"
+// console.log(res);
